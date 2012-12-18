be9deb2e70537aa51411613546b4431eb5db5857
Matthew Tse <matt.s.tse@gmail.com>
2012-12-18 13:31:48 -0500

    added homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RoommateMatch::Application.routes.draw do
+  root :to => 'static_pages#home'
+  match '/home', to: 'static_pages#home'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
