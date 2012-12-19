0a3dec5c0bea83c8f735e923f4b72cbf9c8c9e05
Matthew Tse <matt.s.tse@gmail.com>
2012-12-19 16:29:58 -0500

    added ethnicity modification

--- app/models/ethnicity.rb
+++ app/models/ethnicity.rb
@@ -1,5 +1,5 @@
 class Ethnicity < ActiveRecord::Base
   attr_accessible :name
   has_many :users
-  validates :name, presense:true, uniqueness: {case_sensitive: false }
+  validates :name, presence:true, uniqueness: {case_sensitive: false }
 end
